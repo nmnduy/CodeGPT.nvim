74ef30cf3648b6d5b27f93dc01076a0395046403
duy <nmnduy@gmail.com>
2025-05-05 16:07:41 +0700

    list files

--- lua/codegpt/commands_list.lua
+++ lua/codegpt/commands_list.lua
@@ -88,6 +88,16 @@ function CommandsList.get_cmd_opts(cmd)
     opts = vim.tbl_extend("force", opts, user_set_opts)
     opts = vim.tbl_extend("force", cmd_default, opts)
 
+    if opts.list_files then
+        if vim.fn.executable('rg') == 0 then
+            local files = vim.fn.system('rg --files')
+            local original_message = opts.user_message_template
+            opts.user_message_template = string.format("# Files:\n```\n%s```\n%s", files, original_message)
+        else
+            vim.notify("Warning: 'rg' command not found - skipping file listing", vim.log.levels.WARN)
+        end
+    end
+
     if opts.callback_type == "custom" then
         opts.callback = user_set_opts.callback
     else
